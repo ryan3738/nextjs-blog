4fba2c94ea4ff30b3457f3db6b0dda6bd4d77c24
Ryan <ryan3738@gmail.com>
2021-06-07 15:10:55 -0700

    fix .eslintrc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { AppProps } from 'next/app'
 import SimpleReactLightbox from 'simple-react-lightbox'
+import { ChakraProvider } from '@chakra-ui/react'
 import NavBar from '../components/NavBar'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [open, setOpen] = useState(false)
   return (
-    <NavBar open={open} setOpen={setOpen}>
-      <SimpleReactLightbox>
-        <Component {...pageProps} open={open} setOpen={setOpen} />
-      </SimpleReactLightbox>
-    </NavBar>
+    <ChakraProvider>
+      <NavBar open={open} setOpen={setOpen}>
+        <SimpleReactLightbox>
+          <Component {...pageProps} open={open} setOpen={setOpen} />
+        </SimpleReactLightbox>
+      </NavBar>
+    </ChakraProvider>
   )
 }
